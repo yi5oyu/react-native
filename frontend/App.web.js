984ac3e214ddd1deaabec09260219b58774bfb7d
Young <yi5oyu@gmail.com>
2024-08-13 05:10:01 +0900

    test: A.jsx import
    A.jsx import 테스트

--- frontend/App.web.js
+++ frontend/App.web.js
@@ -1,32 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native-web';
+//import { View, Text, StyleSheet } from 'react-native-web';
+import { B } from './components/A';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native Web!</Text>
-      <Text style={styles.instructions}>To get started, edit App.web.js</Text>
-    </View>
+    <B />
+//    <View style={styles.container}>
+//      <Text style={styles.welcome}>Welcome to React Native Web!</Text>
+//      <Text style={styles.instructions}>To get started, edit App.web.js</Text>
+//    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    justifyContent: 'center',
+//    alignItems: 'center',
+//    backgroundColor: '#F5FCFF',
+//  },
+//  welcome: {
+//    fontSize: 20,
+//    textAlign: 'center',
+//    margin: 10,
+//  },
+//  instructions: {
+//    textAlign: 'center',
+//    color: '#333333',
+//    marginBottom: 5,
+//  },
+//});
 
 export default App;
