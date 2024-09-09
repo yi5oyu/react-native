1ffd4054f0c5a1298a9ef4208e6e2d85ced129ce
Young <yi5oyu@gmail.com>
2024-09-09 18:50:11 +0900

    init: api/instance.js 생성
    axios 인스턴스 생성

---
+++ frontend/api/instance.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'http://localhost:8080',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+})
+
+export default instance
